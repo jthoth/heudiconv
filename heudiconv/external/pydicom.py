033265e30d3a7946148dbe64c924a00a0323f4b3
Jhon Intriago <jhon.thoth@gmail.com>
2024-02-17 16:42:13 -0300

    The final version of pydicom was updated

--- heudiconv/external/pydicom.py
+++ heudiconv/external/pydicom.py
@@ -3,9 +3,5 @@
 from __future__ import absolute_import
 
 # 1.0.0 of pydicom renamed module from dicom to pydicom
-try:
-    # pydicom >= 1.0
-    import pydicom as dcm
-except ImportError:
-    # for pydicom < 1.0
-    import dicom as dcm
+import pydicom as dcm
+
